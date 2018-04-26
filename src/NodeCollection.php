7eed29532f5f4dd112ed2e16161e0585fa017896
battlecook <dreamuj@gmail.com>
2018-04-27 07:17:31 +0900

    add NodeCollection data structure

---
+++ src/NodeCollection.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace battlecook\AStar;
+
+final class NodeCollection
+{
+   private $nodeMap;
+   private $minNode;
+
+   public function __construct()
+   {
+       $this->nodeMap = array();
+   }
+
+   public function get(Node $node): ?Node
+   {
+       if(isset($this->nodeMap[$node->getIndex()]))
+       {
+           return $this->nodeMap[$node->getIndex()];
+       }
+       return null;
+   }
+
+   public function getAll()
+   {
+       return $this->nodeMap;
+   }
+
+   public function add(Node $node)
+   {
+       if(isset($this->nodeMap[$node->getIndex()]) === false)
+       {
+           $this->nodeMap[$node->getIndex()] = $node;
+
+           if($this->minNode === null)
+           {
+               $this->minNode = $node;
+           }
+           else
+           {
+               if($this->minNode->f > $node->f)
+               {
+                   $this->minNode = $node;
+               }
+           }
+       }
+   }
+
+   public function remove(Node $node)
+   {
+       unset($this->nodeMap[$node->getIndex()]);
+   }
+
+   public function count()
+   {
+
+       return count($this->nodeMap);
+   }
+
+   public function getMinFNode(): ?Node
+   {
+       return $this->minNode;
+   }
+}
